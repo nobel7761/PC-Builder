e5cd803c5a03a35b1ee6943f1c52131d80b7058d
nobel7761 <habiburnobel@gmail.com>
2023-08-27 05:42:47 +0600

    github authentication implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ReactElement } from "react";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout =
     (Component as any).getLayout || ((page: ReactElement) => page);
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <SessionProvider session={pageProps.session}>
+      {getLayout(<Component {...pageProps} />)}
+    </SessionProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
 import RootLayout from "@/layout/RootLayout";
 import React, { ReactElement } from "react";
+import { useSession } from "next-auth/react";
 
 const HomePage = () => {
+  const { data: session } = useSession();
   return (
     <div>
       <h1 className="bg-red-500">home</h1>
+      {session && <p>{session?.user?.name}</p>}
     </div>
   );
 };
