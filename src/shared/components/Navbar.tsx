4ccf6c2c5bb762ac81a94c8a4cd1a3d2a01b507f
nobel7761 <habiburnobel@gmail.com>
2023-10-11 14:15:54 +0600

    button updated

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -114,12 +114,11 @@ const Navbar = () => {
           </Listbox>
         </div>
 
-        <span
-          onClick={() => router.push("/pc-build")}
-          className="w-full md:w-[150px] md:px-4 md:mt-0 mt-2 md:py-2 px-3 py-1.5 rounded-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          PC Builder
-        </span>
+        <Link href="/pc-build">
+          <Button className="w-full md:w-[150px] md:px-4 md:mt-0 mt-2 md:py-2 px-3 py-1.5 rounded-md text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            PC Builder
+          </Button>
+        </Link>
 
         {session?.user?.email && (
           <Button
